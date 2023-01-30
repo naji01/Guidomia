57b874e199212173419ee6133dbdb0db3632c43f
Lotfi <lotfi.naji@wafaassurance.co.ma>
2023-01-30 20:42:51 +0100

    Initial Commit

---
+++ Guidomia/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Guidomia
+//
+//  Created by Lotfi on 30/1/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
